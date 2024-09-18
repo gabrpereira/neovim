307fe741fccfde3cdf3e95bc7c4289ae189c317d
Gabriel Pereira <140173310+gabrpereira@users.noreply.github.com>
2024-09-18 13:14:39 -0300

    modified:   init.lua

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
 require("settings")
 require("maps")
 require("lazy.lazy")
-
-local function open_welcome_buffer()
-  vim.cmd('help')
-  vim.cmd('bdelete 1')
-end
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = open_welcome_buffer,
-})
-
