77a8f104dc79aced45bca30598b0a9d34a59ccfc
Gabriel Pereira <140173310+gabrpereira@users.noreply.github.com>
2024-09-18 12:57:37 -0300

    Which-key pluggin added

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
 require("settings")
 require("maps")
 require("lazy.lazy")
+
+local function open_welcome_buffer()
+  vim.cmd('help')
+  vim.cmd('bdelete 1')
+end
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = open_welcome_buffer,
+})
+
